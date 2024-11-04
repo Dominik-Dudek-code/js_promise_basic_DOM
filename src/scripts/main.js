8441b9b4884e26532b3425dbb0bb8964d986e8ba
Dominik Dudek <dom-dud774@o2.pl>
2024-11-04 01:28:34 +0100

    Task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  document.querySelector('.logo').addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+const succesHandler = (message) => {
+  const messageDiv = document.createElement('div');
+
+  messageDiv.className = 'message';
+  messageDiv.textContent = message;
+  document.body.appendChild(messageDiv);
+};
+
+const errorHandler = (error) => {
+  const messageDiv = document.createElement('div');
+
+  messageDiv.className = 'message error-message';
+  messageDiv.textContent = error.message;
+  document.body.appendChild(messageDiv);
+};
+
+promise1.then(succesHandler).catch(errorHandler);
+promise2.then(succesHandler).catch(errorHandler);
